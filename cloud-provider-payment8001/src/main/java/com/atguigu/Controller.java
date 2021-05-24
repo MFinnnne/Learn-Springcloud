e780dd0cfbef7de37960825ed153b4836e87cd0e
MFine <1737374338@qq.com>
2021-05-24 23:58:27 +0800

    just so so

--- cloud-provider-payment8001/src/main/java/com/atguigu/Controller.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/Controller.java
@@ -4,6 +4,9 @@ import com.atguigu.entities.CommonResult;
 import com.atguigu.entities.Payment;
 import com.atguigu.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -20,6 +23,7 @@ public class Controller {
     @Resource
     private PaymentService paymentService;
 
+    @PostMapping("/payment/create")
     public CommonResult<Integer> create(Payment payment) {
         int result = paymentService.create(payment);
         log.info("结果: " + result);
@@ -29,5 +33,18 @@ public class Controller {
 
         }
         return new CommonResult<>(444, "filed", null);
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("******查询结果：" + payment);
+
+        if (payment != null) {
+            //查询成功
+            return new CommonResult<>(200, "查询成功", payment);
+        } else {
+            return new CommonResult<>(444, "没有对应记录，查询ID：" + id);
+        }
     }
 }
