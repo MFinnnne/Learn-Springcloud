dba37daab78aae996939677a64c066b5610e3a9f
MFine <1737374338@qq.com>
2021-05-31 23:57:22 +0800

    zookeeper config

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author MFine
+ * @version 1.0
+ * @date 2021/5/24 23:11
+ **/
+@SpringBootApplication
+public class PaymentMain8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class);
+    }
+}
