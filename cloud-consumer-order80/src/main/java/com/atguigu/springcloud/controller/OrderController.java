9aa31ca2656623b90764f33dee2ae795db818b49
MFine <1737374338@qq.com>
2021-06-09 00:03:11 +0800

    littleModify

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,16 +25,20 @@ public class OrderController {
     private RestTemplate restTemplate;
 
     @PostMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(@RequestBody Payment payment){
+    public CommonResult<Payment> create(@RequestBody Payment payment) {
         log.info("********插入的数据：" + payment);
         //postForObject分别有三个参数：请求地址，请求参数，返回的对象类型
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
     @GetMapping("/consumer/payment/get/{id}")
-    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         log.info("********查询的id：" + id);
         //getForObject两个参数：请求地址，返回的对象类型
-        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        ResponseEntity<CommonResult> forEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (forEntity.getStatusCode().is2xxSuccessful()) {
+            return new CommonResult<>(444, "操作失败")；
+        }
+        return forEntity.getBody();
     }
 }
