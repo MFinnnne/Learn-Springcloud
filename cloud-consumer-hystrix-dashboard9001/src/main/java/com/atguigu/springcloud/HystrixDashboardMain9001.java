c6f4abbbd18d0ae091879883c10ac33a797dd21e
MFine <1737374338@qq.com>
2021-06-16 23:55:56 +0800

    learning hystrix dashboard

--- cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -1,10 +1,10 @@
 package com.atguigu.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -13,11 +13,11 @@ import org.springframework.context.annotation.Bean;
  * @date 2021/5/24 23:11
  **/
 @SpringBootApplication
-@EnableEurekaClient
-@EnableCircuitBreaker
-public class PaymentMain8001 {
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class);
+
+        SpringApplication.run(HystrixDashboardMain9001.class);
     }
 
 
