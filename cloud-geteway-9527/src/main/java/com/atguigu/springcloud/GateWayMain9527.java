3b7a18317e4da33dc3a02421497560c2a8ae6287
MFine <1737374338@qq.com>
2021-06-18 00:01:50 +0800

    learn  getaway

--- cloud-geteway-9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
+++ cloud-geteway-9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -2,22 +2,17 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @author MFine
  * @version 1.0
- * @date 2021/5/24 23:11
+ * @date 2021/6/17 23:47
  **/
 @SpringBootApplication
 @EnableEurekaClient
-public class PaymentMain8001 {
+public class GateWayMain9527 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class);
+        SpringApplication.run(GateWayMain9527.class);
     }
-
-
 }
