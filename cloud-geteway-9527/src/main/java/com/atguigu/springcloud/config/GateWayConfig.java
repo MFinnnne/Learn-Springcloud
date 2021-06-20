d06e581f2426bd567a2ceaee41d605964698b40d
MFine <1737374338@qq.com>
2021-06-20 23:58:15 +0800

    learn  getaway

---
+++ cloud-geteway-9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author MFine
+ * @version 1.0
+ * @date 2021/6/20 23:56
+ **/
+@Configuration
+public class GateWayConfig {
+
+}
