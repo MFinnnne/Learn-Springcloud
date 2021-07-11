820298cbac63e38a739df3649f41e571203979bb
MFine <1737374338@qq.com>
2021-07-12 01:11:37 +0800

    learn sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author MFine
+ * @version 1.0
+ * @date 2021/7/11 23:52
+ **/
+@RestController
+@Slf4j
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA()
+    {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB()
+    {
+        log.info(Thread.currentThread().getName()+"\t"+"...testB");
+        return "------testB";
+    }
+}
